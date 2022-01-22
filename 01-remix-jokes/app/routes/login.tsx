63cd1ee0b7fcf3a4c32fce5e07049ed1b22e1da1
mingyuchoo <mingyuchoo@gmail.com>
2022-01-23 05:18:30 +0900

    31 make set session information to cookie

--- 01-remix-jokes/app/routes/login.tsx
+++ 01-remix-jokes/app/routes/login.tsx
@@ -1,7 +1,7 @@
 import type { ActionFunction, LinksFunction } from "remix";
 import { useActionData, json, Link, useSearchParams } from "remix";
 import { db } from "~/utils/db.server";
-import { login } from "~/utils/session.server";
+import { createUserSession, login } from "~/utils/session.server";
 import stylesUrl from "../styles/login.css";
 
 export const links: LinksFunction = () => {
@@ -63,17 +63,13 @@ export const action: ActionFunction = async ({ request }) => {
   switch (loginType) {
     case "login": {
       const user = await login({ username, password });
-      console.log({ user });
-      if (!user) {
-        return badRequest({
-          fields,
-          formError: `Username/Password combination is incorrect`
-        });
-      }
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+        if (!user) {
+          return {
+            fields,
+            formError: `Username/Password combination is incorrect`
+          };
+        }
+        return createUserSession(user.id, redirectTo);
     }
     case "register": {
       const userExists = await db.user.findFirst({
--- 01-remix-jokes/app/utils/session.server.ts
+++ 01-remix-jokes/app/utils/session.server.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import { createCookieSessionStorage, redirect } from "remix";
 import { db } from "./db.server";
 
 type LoginForm = {
@@ -16,4 +17,34 @@ export async function login({ username, password }: LoginForm) {
   if (!isCorrectPassword) return null;
 
   return user;
+}
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "RJ_session",
+    // normally you want this to be `secure: true`
+    // but that doesn't work on localhost for Safari
+    // https://web.dev/when-to-use-local-https/
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set("userId", userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
 }
