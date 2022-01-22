83f09fcc36878698cae9d95368017483c8220381
mingyuchoo <mingyuchoo@gmail.com>
2022-01-22 12:14:02 +0900

    23 check input validation `/jokes/new` route form

--- 01-remix-jokes/app/routes/jokes/new.tsx
+++ 01-remix-jokes/app/routes/jokes/new.tsx
@@ -1,39 +1,118 @@
 import type { ActionFunction } from "remix"
-import { redirect } from "remix"
+import { useActionData, redirect, json } from "remix";
 import { db } from "~/utils/db.server"
+
+function validateJokeName(name: string) {
+    if (name.length < 2) {
+        return `That joke's name is too short`;
+    }
+}
+
+function validateJokeContent(content: string) {
+    if (content.length < 10) {
+        return `That joke is too short`;
+    }
+}
+
+type ActionData = {
+    formError?: string;
+    fieldErrors?: {
+        name: string | undefined;
+        content: string | undefined;
+    };
+    fields?: {
+        name: string;
+        content: string;
+    }
+}
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
+
 
 export const action: ActionFunction = async ({ request }) => {
     const form = await request.formData();
     const name = form.get("name")
     const content = form.get("content")
-    // we do this type check to be extra sure and to make TypeScript happy
-    // we'll explore validation next!
     if (
         typeof name !== "string" ||
         typeof content !== "string"
     ) {
-        throw new Error(`Form not submitted correctly.`);
+        return badRequest({
+            formError: `Form not submmited correctly`
+        })
     }
 
+    const fieldErrors = {
+        name: validateJokeName(name),
+        content: validateJokeContent(content)
+    }
     const fields = { name, content }
+    if (Object.values(fieldErrors).some(Boolean)) {
+        return badRequest({ fieldErrors, fields });
+    }
     const joke = await db.joke.create({ data: fields });
     return redirect(`/jokes/${joke.id}`);
 };
 
 export default function NewJokeRoute() {
+    const actionData = useActionData<ActionData>();
     return (
         <div>
             <p>Add your own hilarious joke</p>
             <form method="post">
                 <div>
                     <label>
-                        Name: <input type="text" name="name" />
+                        Name:
+                        <input
+                            type="text"
+                            name="name"
+                            defaultValue={actionData?.fields?.name}
+                            aria-onInvalid={
+                                Boolean(actionData?.fieldErrors?.name) || undefined
+                            }
+                            aria-describedby={
+                                actionData?.fieldErrors?.name
+                                    ? "name-error"
+                                    : undefined
+                            }
+                        />
                     </label>
+                    {actionData?.fieldErrors?.name
+                        ? (
+                            <p
+                                className="form-validation-error"
+                                role="alert"
+                                id="name-error"
+                            >
+                                {actionData.fieldErrors.name}
+                            </p>
+                        ) : null }
                 </div>
                 <div>
                     <label>
-                        Content: <textarea name="content" />
+                        Content:
+                        <textarea
+                            name="content"
+                            defaultValue={actionData?.fields?.content}
+                            aira-invalid={
+                                Boolean(actionData?.fieldErrors?.content) || undefined
+                            }
+                            aria-describedby={
+                                actionData?.fieldErrors?.content
+                                    ? "content-error"
+                                    : undefined
+                            }
+                        />
                     </label>
+                    {actionData?.fieldErrors?.content
+                        ? (
+                            <p className="form-validation-error"
+                            role="alert"
+                            id="content-error"
+                            >
+                                {actionData.fieldErrors.content}
+                            </p>
+                        ): null }
                 </div>
                 <div>
                     <button type="submit" className="button">add</button>
