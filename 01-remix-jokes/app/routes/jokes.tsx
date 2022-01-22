39e2f9599b251173389981efebddf554bcc6c247
mingyuchoo <mingyuchoo@gmail.com>
2022-01-23 05:36:15 +0900

    33 build logout action

--- 01-remix-jokes/app/routes/jokes.tsx
+++ 01-remix-jokes/app/routes/jokes.tsx
@@ -1,6 +1,8 @@
+import { User } from "@prisma/client";
 import { LinksFunction, LoaderFunction } from "remix";
 import { Link, Outlet, useLoaderData } from "remix";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
 import styleUrl from "../styles/jokes.css"
 
 export const links: LinksFunction = () => {
@@ -13,19 +15,26 @@ export const links: LinksFunction = () => {
 }
 
 type LoaderData = {
+    user: User | null;
     jokeListItems: Array<{ id: string; name: string }>;
 }
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({
+    request
+}) => {
+    const jokeListItems = await db.joke.findMany({
+        take: 5,
+        orderBy: { createdAt: "desc" },
+        select: { id: true, name: true }
+    });
+    const user = await getUser(request);
+
     const data: LoaderData = {
-        jokeListItems: await db.joke.findMany({
-            take: 5,
-            select: { id: true, name: true },
-            orderBy: { createdAt: "desc" },
-        })
-    }
+        jokeListItems,
+        user
+    };
     return data;
-}
+};
 
 export default function JokesRoute() {
     const data = useLoaderData<LoaderData>();
@@ -39,6 +48,18 @@ export default function JokesRoute() {
                             <span className="logo-medium">J🤪KES</span>
                         </Link>
                     </h1>
+                    {data.user ? (
+                        <div className="user-info">
+                        <span>{`Hi ${data.user.username}`}</span>
+                        <form action="/logout" method="post">
+                            <button type="submit" className="button">
+                                Logout
+                            </button>
+                        </form>
+                        </div>
+                    ) : (
+                        <Link to="/login">Login</Link>
+                    )}
                 </div>
             </header>
             <main className="jokes-main">
